739d14ae381de92cc0040db1eb75484af77edb0d
Lunar-Academy <116317119+HasiP145@users.noreply.github.com>
2023-02-04 11:55:23 -0500

    Got Video Feed Working With a USB Camera

---
+++ Vision.py
@@ -0,0 +1,61 @@
+# Import the camera server
+from cscore import CameraServer
+
+# Import OpenCV and NumPy
+import cv2
+import numpy as np
+print('q')
+def main():
+    print('w')
+    #cs = CameraServer.getInstance()
+    # camera = CameraServer.startAutomaticCapture()
+    # camera.setResolution(1200,720)
+    # CameraServer.enableLogging()
+    print('camera')
+    # cs = CameraServer.startAutomaticCapture()
+    # cs = CameraServer.getInstance()
+    # cs.
+    cs = CameraServer.getServer()
+    # cs.enumerateSinks
+    # CameraServer.addCamera()
+    print(cs.getConfigJson())
+
+    # Capture from the first USB Camera on the system
+    camera = CameraServer.startAutomaticCapture(dev=1)
+
+    camera.setResolution(320, 240)
+
+
+    # Capture from the first USB Camera on the system
+    # camera = CameraServer.startAutomaticCapture()
+    # camera.setResolution(320, 240)
+
+    # Get a CvSink. This will capture images from the camera
+    cvSink = CameraServer.getVideo()
+    # print(cvSink)
+
+    # (optional) Setup a CvSource. This will send images back to the Dashboard
+    outputStream = CameraServer.putVideo("Name", 320, 240)
+
+    # Allocating new images is very expensive, always try to preallocate
+    img = np.zeros(shape=(240, 320, 3), dtype=np.uint8)
+
+    while True:
+        # Tell the CvSink to grab a frame from the camera and put it
+        # in the source image.  If there is an error notify the output.
+        time, img = cvSink.grabFrame(img)
+        cv2.imshow("Faces found", img)
+        cv2.waitKey(1)
+        if time == 0:
+            # Send the output the error.
+            outputStream.notifyError(cvSink.getError());
+            # skip the rest of the current iteration
+            continue
+
+        #
+        # Insert your image processing logic here!
+        #
+
+        # (optional) send some image back to the dashboard
+        outputStream.putFrame(img)
+main()
